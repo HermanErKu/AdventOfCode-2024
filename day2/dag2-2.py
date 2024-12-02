1ad9bb594a706d6949bb1d252022964973e63b2c
Herman Lindalen Nodeland <afs0027473@agderskolen.no>
2024-12-02 08:31:01 +0100

    did first day2 challenge moved all solutions to own folder

---
+++ day2/dag2-2.py
@@ -0,0 +1,44 @@
+def checkIfAllIncreasing(array):
+    pointer = int(array[0])
+    for i in range(1, len(array)):
+        num = int(array[i])
+        if num >= pointer:
+            pointer = num
+        else:
+            return False
+    return True
+
+def checkIfAllDecreasing(array):
+    array = array[::-1]
+    pointer = int(array[0])
+    for i in range(1, len(array)):
+        num = int(array[i])
+        if num >= pointer:
+            pointer = num
+        else:
+            return False
+    return True
+
+def checkIfIncreasingOrDecreasingByCorrectAmount(array):
+    pointer = int(array[0])
+    for i in range(1, (len(array))):
+        num = int(array[i])
+        difference = abs(pointer-num)
+        if (difference <= 3 and difference >= 1):
+            pointer = num
+        else:
+            return False
+    return True
+
+sum = 0
+
+with open('dag2input.txt','r') as file:
+    for line in file.readlines():
+        line = line.split("\n")[0]
+        numbers = line.split(" ")
+        
+        if ((checkIfAllDecreasing(numbers) or checkIfAllIncreasing(numbers)) and checkIfIncreasingOrDecreasingByCorrectAmount(numbers)):
+            sum += 1
+            print(numbers)
+
+print(sum)
