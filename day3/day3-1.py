2f7e649f3fcdf43fc21d78e52a1d7df2717ca16b
Herman Lindalen Nodeland <afs0027473@agderskolen.no>
2024-12-03 14:39:37 +0100

    did day 3 part 1

---
+++ day3/day3-1.py
@@ -0,0 +1,19 @@
+import re
+
+wholeLine = ""
+with open('input.txt', 'r') as file:
+    for line in file:
+        wholeLine += line
+
+regexSearchResult = re.findall('mul\([0-9]{1,3},[0-9]{1,3}\)',wholeLine)
+
+sum = 0
+for result in regexSearchResult:
+    numbers = str(result).split("(")[1].split(")")[0]
+    
+    num1 = int(numbers.split(",")[0])
+    num2 = int(numbers.split(",")[1])
+
+    sum += (num1 * num2)
+
+print(sum)
